b934a94502351b7346c4c879d78b0f94f0b906f9
kelpin <kelpin145@gmail.com>
2024-11-13 21:04:55 +0700

    update leireg

--- assets/js/custome.js
+++ assets/js/custome.js
@@ -1,36 +1,14 @@
-const initSlider = () => {
+var myIndex = 0;
+carousel();
 
-    const imageList = document.querySelector(".slider-wrapper .image-list");
-    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
-    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
-
-    // Handle slide button clicks
-    slideButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const direction = button.id === "prev-slide" ? -1 : 1;
-
-            // Calculate the amount to scroll (2 images at a time)
-            const imageWidth = imageList.querySelector('.image-item').offsetWidth + 10; // Adjust for the gap
-            const scrollAmount = imageWidth * 2 * direction; 
-
-            // Scroll the image list
-            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
-        });
-    });
-
-    // Show or hide slide buttons based on scroll position
-    const handleSlideButtons = () => {
-        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
-        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
-    }
-
-    // Add scroll event listener to update scrollbar thumb and slide buttons
-    imageList.addEventListener("scroll", () => {
-        // updateScrollThumbPosition();
-        handleSlideButtons();
-    });
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 9000);    
 }
-
-// Initialize slider on window load and resize
-window.addEventListener("resize", initSlider);
-window.addEventListener("load", initSlider);
